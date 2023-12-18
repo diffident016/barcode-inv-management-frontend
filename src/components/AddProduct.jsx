624be8fb82735002f67b6325a4fb9ddba6927aa0
diffident016 <marvin.tagolimotjr13@gmail.com>
2023-12-19 00:43:47 +0800

    feat: initial inventory page

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,21 +1,127 @@
-import React, { useState } from 'react'
-import addproduct from '../assets/images/add.png'
-import { PlusIcon, XMarkIcon, PhotoIcon } from '@heroicons/react/24/outline'
-import TextField from '@mui/material/TextField';
+import React, { useState, useReducer, useRef, useMemo } from 'react'
+import { PlusIcon, XMarkIcon, PhotoIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
+import category from '../assets/data/category.json'
 
 function AddProduct({ close }) {
 
-    const [product, setProduct] = useState({
-        name: "",
-        description: "",
-        photoUrl: "",
-        price: 0,
-        stock: 0,
-        cost: 0,
-        category: "",
-        barcode: "",
-        vendor: ""
-    });
+    const hiddenFileInput = useRef(null);
+    const [fileError, setFileError] = useState("");
+
+    const [product, updateProduct] = useReducer((prev, next) => {
+        return { ...prev, ...next }
+    },
+        {
+            name: "",
+            description: "",
+            photoUrl: null,
+            price: "",
+            stock: "",
+            cost: "",
+            category: "",
+            barcode: "",
+            vendor: ""
+        });
+
+    const categories = useMemo(() => category['categories'].map((cat) => {
+        return {
+            id: cat["category_id"],
+            label: cat["category_name"],
+            value: cat["category_name"].toLowerCase()
+        }
+    }), [category]);
+
+    const Field = (label, value, field, isCurrency = false) => {
+
+        if (isCurrency) {
+            return (
+                <div className='flex flex-col w-full'>
+                    <label className='pb-1 text-xs font-lato-bold text-[#555C68]/80'>{label}</label>
+                    <div className='flex flex-row items-center w-full h-8 rounded-md focus:outline-none border border-[#555C68]/50 text-sm px-2'>
+                        <p className='text-[#555C68]/80'>&#x20B1;</p>
+                        <input
+                            required
+                            value={value}
+                            placeholder='0'
+                            min={0}
+                            type='number'
+                            onChange={(e) => { updateProduct({ [field]: e.target.value }) }}
+                            className='w-full h-8 focus:outline-none bg-transparent text-end' />
+                    </div>
+
+                </div>);
+        }
+        return (
+            <div className='flex flex-col w-full'>
+                <label className='pb-1 text-xs font-lato-bold text-[#555C68]/80'>{label}</label>
+                <input
+                    required
+                    value={value}
+                    onChange={(e) => { updateProduct({ [field]: e.target.value }) }}
+                    className='w-full h-8 rounded-md focus:outline-none border border-[#555C68]/50 text-sm px-2' />
+            </div>
+        )
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!product.photoUrl || product.photoUrl == '') {
+            setFileError('Please add a product image.')
+            return
+        }
+    }
+
+    const handleReset = (event) => {
+        event.preventDefault();
+        Object.keys(product).forEach((inputKey) => {
+            updateProduct({ [inputKey]: ['price', 'cost', 'stock'].includes(inputKey) ? 0 : '' });
+        });
+
+        close();
+    };
+
+    const handleClick = () => {
+        hiddenFileInput.current.click();
+    };
+
+    const handleChange = event => {
+
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        const fileUploaded = event.target.files[0];
+
+        if (!allowedTypes.includes(fileUploaded.type)) {
+            setFileError('Invalid image file.')
+            return
+        }
+        setFileError(null)
+        updateProduct({ photoUrl: fileUploaded })
+    };
+
+    const imageDisplay = (photo) => {
+
+        if (photo != null || photo == "") {
+
+            console.log(product.photoUrl)
+            return (
+                <div className='relative w-full h-full rounded-lg'>
+                    <div className='absolute z-10 opacity-0 hover:opacity-100 bg-black/40 w-[140px] h-[120px] rounded-lg'>
+                        <div className='flex flex-row items-center text-white justify-center h-full gap-2 cursor-pointer select-none'>
+                            <PencilSquareIcon className='w-5' />
+                            <p className='text-xs font-lato-bold flex flex-row gap-1 items-center'>Edit Image</p>
+                        </div>
+                    </div>
+                    <img src={URL.createObjectURL(product.photoUrl)} className='object-cover w-[140px] h-[120px] rounded-lg' />
+
+                </div>
+            );
+        }
+        return (
+            <div className='flex flex-col items-center justify-center h-full gap-2 cursor-pointer select-none'>
+                <PhotoIcon className='w-8 ' />
+                <p className='text-xs font-lato-bold flex flex-row gap-1 items-center'><span><PlusIcon className='w-3 h-3' /></span>Add image</p>
+            </div>
+        );
+    }
 
     return (
         <div className='w-[500px] h-[520px] bg-white rounded-lg text-[#555C68]'>
@@ -24,48 +130,55 @@ function AddProduct({ close }) {
                     <h1 className='flex gap-2 items-center font-lato-bold'><span><PlusIcon className='w-5' /></span>Add new Product</h1>
                     <XMarkIcon onClick={() => { close() }} className='w-5 cursor-pointer' />
                 </div>
-                <form className='flex flex-col p-4 w-full h-full'>
+                <form onSubmit={handleSubmit} onReset={handleReset} className='flex flex-col p-4 w-full h-full'>
                     <div className='flex flex-row gap-4'>
-                        <div className='w-[140px] cursor-pointer text-[#555C68]/70 h-[120px] rounded-lg border gap-2 select-none flex flex-col items-center justify-center'>
-                            <PhotoIcon className='w-8 ' />
-                            <p className='text-xs font-lato-bold flex flex-row gap-1 items-center'><span><PlusIcon className='w-3 h-3' /></span>Add image</p>
+                        <div onClick={handleClick} className='w-[140px] text-[#555C68]/70 h-[120px] rounded-lg border-[#555C68]/50 border '>
+                            <input
+                                onChange={handleChange}
+                                ref={hiddenFileInput}
+                                type='file'
+                                className='hidden'
+                                accept='image/png, image/gif, image/jpeg' />
+                            {imageDisplay(product.photoUrl)}
                         </div>
                         <div className='flex flex-col flex-1 h-full w-full gap-2'>
-                            <div className='flex flex-col'>
-                                <label className='pb-1 text-xs font-lato-bold text-[#555C68]/80'>Product Name</label>
-                                <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
-                            </div>
-                            <div className='flex flex-col'>
+                            {Field("Product Name", product.name, 'name')}
+                            <div className='flex flex-col w-full'>
                                 <label className='pb-1 text-xs font-lato-bold text-[#555C68]/80'>Category</label>
-                                <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
+                                <div className='w-full h-8 rounded-md focus:outline-none border border-[#555C68]/50 px-1'>
+                                    <select
+                                        onChange={(e) => { console.log(e.target.value) }}
+                                        required
+                                        className='w-full h-full focus:outline-none bg-transparent text-sm '>
+                                        {categories.map((item) => <option id={item.id} value={item.value}>{item.label}</option>)}
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-row gap-2'>
-                        <div className='flex-1 flex flex-col'>
-                            <label className='p-1 mt-1 text-xs font-lato-bold text-[#555C68]/80'>Price</label>
-                            <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
-                        </div>
-                        <div className='flex-1 flex flex-col'>
-                            <label className='p-1 mt-1 text-xs font-lato-bold text-[#555C68]/80'>Cost</label>
-                            <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
-                        </div>
+                    <div className='flex flex-row gap-2 mt-2'>
+                        {Field("Price", product.price, 'price', true)}
+                        {Field("Cost", product.cost, 'cost', true)}
                     </div>
-                    <div className='flex flex-row gap-2'>
-                        <div className='flex-1 flex flex-col'>
-                            <label className='p-1 mt-1 text-xs font-lato-bold text-[#555C68]/80'>Manufacturer</label>
-                            <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
-                        </div>
+                    <div className='flex flex-row gap-2 mt-2'>
+                        {Field("Manufacturer", product.manufacturer, 'manufacturer')}
                         <div className='w-28 flex flex-col'>
-                            <label className='p-1 mt-1 text-xs font-lato-bold text-[#555C68]/80'>Stock</label>
-                            <input className='w-full h-8 rounded-md focus:outline-none border text-sm px-2' />
+                            <label className='pb-1 text-xs font-lato-bold text-[#555C68]/80'>Stock</label>
+                            <input
+                                required
+                                type='number'
+                                min={0}
+                                value={product.stock}
+                                onChange={(e) => { updateProduct({ stock: e.target.value }) }}
+                                className='w-full h-8 rounded-md focus:outline-none border border-[#555C68]/50 text-sm px-2' />
                         </div>
                     </div>
                     <label className='mt-2 p-1 text-xs font-lato-bold text-[#555C68]/80'>Description</label>
-                    <textarea placeholder='Add product description' rows={4} className='text-sm p-2 border rounded-md resize-none focus:outline-none' />
+                    <textarea placeholder='Add product description' rows={4} className='text-sm p-2 border-[#555C68]/50 border rounded-md resize-none focus:outline-none' />
                     <div className='flex flex-row w-full gap-5 px-2 my-5 justify-end'>
-                        <button className='bg-[#555C68] rounded-md p-2 text-sm text-white'>Add Product</button>
-                        <button className='font-lato-bold text-sm text-[#555C68]'>Cancel</button>
+                        {fileError && <p className='flex-1 text-sm text-[#ff3333]'>{fileError}</p>}
+                        <button type='submit' className='bg-[#555C68] rounded-md p-2 text-sm text-white'>Add Product</button>
+                        <button type='reset' className='font-lato-bold text-sm text-[#555C68]'>Cancel</button>
                     </div>
                 </form>
             </div>
--- src/screens/admin/Inventory.jsx
+++ src/screens/admin/Inventory.jsx
@@ -5,52 +5,64 @@ import {
 import barcode from '../../assets/images/barcode.png'
 import Backdrop from '@mui/material/Backdrop';
 import AddProduct from '../../components/AddProduct';
+import DataTable from "react-data-table-component";
+import Loader from '../../components/Loader';
+import sameleProducts from '../../assets/data/products.json'
 
 function Inventory() {
 
     const [query, setQuery] = useState("");
     const [addProduct, setAddProduct] = useState(false);
 
+    const products = useMemo(() => sameleProducts['products'].map((item) => item), [sameleProducts])
+
     const columns = useMemo(
         () => [
             {
-                name: "Student ID",
-                selector: (row) => row.student.studentId,
+                name: "#",
+                cell: (row) => {
+                    return (
+                        <div className='flex flex-row h-full w-full'>
+                            <div className='w-6'>
+
+                            </div>
+                            <div className='p-[1px] w-12 h-12 rounded-lg border'>
+                                <img className='w-full h-full rounded-lg object-cover' src={row.photoUrl} alt='product photo' />
+                            </div>
+
+                        </div>
+                    )
+                },
+                width: '120px'
+            },
+            {
+                name: "SKU",
+                selector: (row) => "000",
+                width: '120px'
+            },
+            {
+                name: "Product Name",
+                selector: (row) => row.product_name,
+                width: '250px'
+            },
+            {
+                name: "Category",
+                selector: (row) => row.category,
+                width: '180px'
+            },
+            {
+                name: "Price",
+                selector: (row) => row.price,
                 width: '100px'
             },
             {
-                name: "Name",
-                selector: (row) => row.student.name,
-                width: '200px'
+                name: "Cost",
+                selector: (row) => row.cost,
+                width: '100px'
             },
             {
-                name: "Date Record",
-                selector: (row) => format(row.dateRecord.toDate(), 'LL/dd/yyyy'),
-                width: '110px'
-            },
-            {
-                name: "Time",
-                selector: (row) => format(row.dateRecord.toDate(), 'hh:mm a'),
-                width: '110px'
-            },
-            {
-                name: "Status",
-                cell: function (row) {
-
-                    return (
-                        row.status == 0 ?
-                            <div className="flex bg-[#339655] rounded-sm items-center justify-center w-[90px] h-[20px] cursor-pointer">
-                                <p className="font-roboto-bold text-white text-xs">
-                                    INSIDE
-                                </p>
-                            </div> :
-                            <div className="flex bg-[#fb0200] rounded-sm items-center justify-center w-[90px] h-[20px] cursor-pointer">
-                                <p className="font-roboto-bold text-white text-xs">
-                                    OUTSIDE
-                                </p>
-                            </div>
-                    )
-                },
+                name: "Stock",
+                selector: (row) => row.stock,
                 width: '100px'
             },
         ]
@@ -58,7 +70,7 @@ function Inventory() {
 
     return (
         <div className='flex flex-row w-full h-full p-4 text-[#555C68] gap-4'>
-            <div className='flex flex-col flex-1 h-full rounded-lg border bg-white p-4'>
+            <div className='flex flex-col flex-1 h-full rounded-lg border bg-white p-6 overflow-hidden'>
                 <h1 className='font-lato-black text-base'>Inventory</h1>
                 <div className='flex flex-row justify-between w-full items-center pt-4'>
                     <input
@@ -66,7 +78,6 @@ function Inventory() {
                         onChange={(e) => {
                             const query = e.target.value
                             setQuery(query)
-
                         }}
                         className='px-2 text-sm rounded-md h-8 w-52 border focus:outline-none'
                         placeholder='Quick Search' />
@@ -83,18 +94,44 @@ function Inventory() {
                     <Backdrop
                         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                         open={addProduct}
-                    // onClick={() => {
-                    //     setAddProduct(false)
-                    // }}
                     >
                         <AddProduct close={() => { setAddProduct(false) }} />
                     </Backdrop>
                 </div>
-
+                <div className='flex flex-col w-full h-full px-2'>
+                    <div className='flex flex-row w-full h-14 border-b items-center'>
+                        {
+                            columns.map((item) => {
+                                return (
+                                    <p style={{ width: item.width }} className='text-[#555C68]/80 text-sm font-lato-bold mx-1'>{item.name}</p>
+                                );
+                            })
+                        }
+                    </div>
+                    <div className='flex flex-col overflow-auto h-full w-full pb-12'>
+                        {
+                            products.map((item) => {
+                                return (
+                                    <div className='flex flex-row w-full h-16 border-b py-2'>
+                                        {
+                                            columns.map((col) => {
+                                                return (
+                                                    <div style={{ width: col.width }} className='overflow-ellipsis mx-1 h-full flex flex-row items-center'>
+                                                        {col.cell ? col.cell(item)
+                                                            : <p className='w-full font-lato-bold text-sm overflow-hidden text-ellipsis'>{col.selector(item)}</p>}
+                                                    </div>
+                                                )
+                                            })}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
             </div>
-            <div className='w-[250px] h-full rounded-lg border bg-white p-4'>
+            {/* <div className='w-[200px] h-full rounded-lg border bg-white p-4'>
                 <h1 className='font-lato-black text-base'>Overview</h1>
-            </div>
+            </div> */}
         </div>
     )
 }
